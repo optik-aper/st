a2289ccfdaeec33816edae68e52e72da0ca3989f
Michael Riley <michael@tsuga.dev>
2025-03-26 19:06:15 -0400

    Set font to envypn

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "lucy tewii:style=regular";
+static char *font = "envypn:style=Regular";
 static int borderpx = 6;
 
 /*
